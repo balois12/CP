9ae5b12b3cbf7bb5e9594f2d72fdd5a8718c0ec1
balois12 <67746553+balois12@users.noreply.github.com>
2024-05-03 08:15:43 -0500

    Add files via upload

---
+++ CP.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+#st.page_link("pages/Materiales.py", label="Materiales", icon="⚓")
+#st.page_link("pages/Esfuerzo_adicional.py", label="Esfuerzo adicional", icon="👨‍🏭")
+#st.page_link("http://www.google.com", label="Google", icon="🌎")
