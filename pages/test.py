e9b1587955e9f97021b46cd22dd754404e9ea3be
balois12 <67746553+balois12@users.noreply.github.com>
2024-05-08 16:27:51 -0500

    Add files via upload

--- pages/test.py
+++ pages/test.py
@@ -1,14 +1,19 @@
 import streamlit as st
-import subprocess
+import pandas as pd
+import numpy as np
+import altair as alt
 
-# Define la función para ejecutar un script de Python
-def run_python_script():
-    # Ruta al script de Python que deseas ejecutar
-    script_path = "SAP.py"
-    # Ejecutar el script de Python
-    subprocess.Popen(["python", script_path], shell=True)
+DATA = 'DATA2GPT.xlsx'
+PD_DATA = pd.read_excel(DATA,sheet_name='DATA').drop('Hoja de entrada', axis=1).drop('Index', axis=1).drop('Liquidación', axis=1).drop('PEP', axis=1).drop('Elem.PEP', axis=1)
 
-st.title("Ejecutar Script de Python con Streamlit")
-if st.button("Ejecutar Script"):
-    run_python_script()
-    st.success("El script de Python está ejecutándose!")
+title = st.text_input("Ingrese la actividad a buscar")
+
+#st.write("The current movie title is", title)
+
+# Filtrar el DataFrame basado en la entrada del usuario
+if title:
+    resultados = PD_DATA[PD_DATA['Descripción Operación'].str.contains(title, case=False)].reset_index(drop=True)
+    st.write('Resultados de la búsqueda:')
+    st.write(resultados,width=900,heitgh=1200)
+else:
+    st.write('Ingresa un nombre para buscar.')
